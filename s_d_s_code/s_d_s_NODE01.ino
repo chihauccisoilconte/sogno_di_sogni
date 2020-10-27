e28d798f25dee9b89c557960b5a975495b73395b
Chi ha ucciso Il Conte? <chihauccisoilconte@gmail.com>
2020-10-27 15:14:20 +0100

    Add files via upload

---
+++ s_d_s_code/s_d_s_NODE01.ino
@@ -0,0 +1,57 @@
+/*
+  Sogno_di_sogni_NODE0 - == Node 01==
+
+  by Chi ha ucciso Il Conte? (Nicolo Merendino - https://chihauccisoilconte.eu/)
+  from code by Dejan, www.HowToMechatronics.com
+  Libraries:
+  nRF24/RF24, https://github.com/nRF24/RF24
+  nRF24/RF24Network, https://github.com/nRF24/RF24Network
+*/
+#include <RF24Network.h>
+#include <RF24.h>
+#include <SPI.h>
+#include<Wire.h>
+
+#define button 3
+RF24 radio(7, 8);               // nRF24L01 (CE,CSN)
+RF24Network network(radio);      // Include the radio in the network
+const uint16_t this_node = 01;   // Address of this node in Octal format ( 04,031, etc)
+const uint16_t node00 = 00;      // Address of the other node in Octal format
+const int MPU = 0x68;
+int16_t AcX, AcY, AcZ1, Tmp, GyX, GyY, GyZ;
+void setup() {
+  Wire.begin();
+  Wire.beginTransmission(MPU);
+  Wire.write(0x6B);
+  Wire.write(0);
+  Wire.endTransmission(true);
+  SPI.begin();
+  radio.begin();
+  network.begin(90, this_node);  //(channel, node address)
+  radio.setDataRate(RF24_2MBPS);
+  pinMode(button, INPUT_PULLUP);
+}
+void loop() {
+  network.update();
+  Wire.beginTransmission(MPU);
+  Wire.write(0x3B);  
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU,12,true);  
+  AcX=Wire.read()<<8|Wire.read();    
+  AcY=Wire.read()<<8|Wire.read();  
+  AcZ1=Wire.read()<<8|Wire.read();  
+  GyX=Wire.read()<<8|Wire.read();  
+  GyY=Wire.read()<<8|Wire.read();  
+  GyZ=Wire.read()<<8|Wire.read(); 
+
+
+  
+  //===== Sending =====//
+
+  // LED Control at Node 01
+ unsigned long acc1value = AcZ1;
+  RF24NetworkHeader header(node00);    // (Address where the data is going)
+  bool ok = network.write(header, &acc1value, sizeof(acc1value)); // Send the data
+
+
+}
---
+++ s_d_s_code/s_d_s_receiver__NODE00_midi_nano.ino
@@ -0,0 +1,141 @@
+/*
+  Sogno_di_sogni_NODE0 - == Base/ Master Node 00==
+
+  by Chi ha ucciso Il Conte? (Nicolo Merendino - https://chihauccisoilconte.eu/)
+  from code by Dejan, www.HowToMechatronics.com
+  Libraries:
+  nRF24/RF24, https://github.com/nRF24/RF24
+  nRF24/RF24Network, https://github.com/nRF24/RF24Network
+*/
+#include <RF24Network.h>
+#include <RF24.h>
+#include <SPI.h>
+#include "MIDIUSB.h"
+
+#define led 4
+#define led2 5
+#define led3 6
+#define led4 7
+
+
+
+RF24 radio(9, 10);               // nRF24L01 (CE,CSN)
+RF24Network network(radio);      // Include the radio in the network
+const uint16_t this_node = 00;   // Address of our node in Octal format ( 04,031, etc)
+const uint16_t node01 = 01;  // Address of the other node in Octal format
+const uint16_t node02 = 02;
+const uint16_t node03 = 03;
+const uint16_t node04 = 04;
+
+
+
+
+void setup() {
+  MIDI.begin(MIDI_CHANNEL_OMNI);
+  Serial.begin(115200);
+  SPI.begin();
+  radio.begin();
+  network.begin(90, this_node);  //(channel, node address)
+  radio.setDataRate(RF24_2MBPS);
+}
+
+void controlChange(byte channel, byte control, byte value) {
+  midiEventPacket_t event = {0x0B, 0xB0 | channel, control, value};
+  MidiUSB.sendMIDI(event);
+}
+
+void loop() {
+  int mc1 = 0;
+  int mc2  = 0;
+  int mc3  = 0;
+  int mc4  = 0;
+
+  network.update();
+  //===== Receiving =====//
+  while ( network.available() ) {     // Is there any incoming data?
+    RF24NetworkHeader header;
+    unsigned long incomingData;
+    network.read(header, &incomingData, sizeof(incomingData)); // Read the incoming data
+
+    // PWM output to LED 01 (dimming)
+    if (header.from_node == 1) {
+      if (incomingData > 5000) {
+        digitalWrite(led, HIGH);
+        controlChange(0, 0, mc1);
+        MidiUSB.flush();
+
+      } else {
+        digitalWrite(led, LOW);
+        mc1 = 0;
+      }
+      if (incomingData < -5000) {
+        mc1 = map(incomingData, -5000, -17000, 0, 127);
+        controlChange(0, 0, mc1);
+        MidiUSB.flush();
+        digitalWrite(led, HIGH);
+      } else {
+        digitalWrite(led, LOW);
+        mc1 = 0;
+      }
+    }
+
+    if (header.from_node == 2) {
+      if (incomingData > 5000) {
+        mc2 = map(incomingData, 5000, 17000, 0, 127);
+        controlChange(0, 1, mc2);
+        MidiUSB.flush();
+        digitalWrite(led2, HIGH);
+      } else {
+        digitalWrite(led2, LOW);
+      }
+      if (incomingData < -5000) {
+        mc2 = map(incomingData, -5000, -17000, 0, 127);
+        controlChange(0, 1, mc2);
+        MidiUSB.flush();
+        digitalWrite(led2, HIGH);
+      } else {
+        digitalWrite(led2, LOW);
+      }
+
+    }
+    if (header.from_node == 3) {
+      if (incomingData > 5000) {
+        mc3 = map(incomingData, 5000, 17000, 0, 127);
+        controlChange(0, 2, mc3);
+        MidiUSB.flush();
+        digitalWrite(led3, HIGH);
+      } else {
+        digitalWrite(led3, LOW);
+      }
+      if (incomingData < -5000) {
+        mc2 = map(incomingData, -5000, -17000, 0, 127);
+        controlChange(0, 2, mc3);
+        MidiUSB.flush();
+        digitalWrite(led3, HIGH);
+      } else {
+        digitalWrite(led3, LOW);
+      }
+    }
+
+    if (header.from_node == 4) {
+      if (incomingData > 5000) {
+        mc3 = map(incomingData, 5000, 17000, 0, 127);
+        controlChange(0, 3, mc4);
+        MidiUSB.flush();
+        digitalWrite(led4, HIGH);
+      } else {
+        digitalWrite(led4, LOW);
+      }
+      if (incomingData < -5000) {
+        mc2 = map(incomingData, -5000, -17000, 0, 127);
+        controlChange(0, 3, mc4);
+        MidiUSB.flush();
+        digitalWrite(led4, HIGH);
+      } else {
+        digitalWrite(led4, LOW);
+      }
+    }
+
+  }
+}
+//this machine kills fascists
